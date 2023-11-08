e4256784f5938d2fbf08279e8355dda2f6a5fd07
Oleh Andrus <oleh.andrus@startpagina.nl>
2023-11-08 13:40:32 +0200

    module 01 and readme

---
+++ main.py
@@ -0,0 +1,41 @@
+# ctrl (option) + shift + f -> search
+# ctrl + O -> open file
+# ctrl + shift + p 
+# breakpoint -> точка зупину
+
+# doc string => multiple line
+
+# статична типізація
+# динамічна типізація
+
+"""
+name = "Oleh"
+print(id(name))
+
+name = "Ivan"
+print(id(name))
+name = "Oleh"
+print(id(name))
+
+name = "Ivan"
+print(id(name))
+name = "Oleh"
+print(id(name))
+
+name = "Ivan"
+print(id(name))
+name = "Oleh"
+print(id(name))
+
+name = "Ivan"
+print(id(name))
+name = "Oleh"
+print(id(name))
+
+name = "Ivan"
+print(id(name))
+
+"""
+
+
+print(None)
